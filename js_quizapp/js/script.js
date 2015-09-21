e2fa2f3e455fc1a6432a88c42270627ea130af1f
Ben Stein <bumstain@gmail.com>
2015-09-21 23:35:24 +0100

    Added correct answers and checks for user input

--- js_quizapp/js/script.js
+++ js_quizapp/js/script.js
@@ -2,6 +2,8 @@ function submitAnswers() {
 	var total = 5;
 	score = 0;
 
+	
+
 	// Get user input
 	var q1 = document.forms["quizForm"]["q1"].value;
 	var q2 = document.forms["quizForm"]["q2"].value;
@@ -9,9 +11,9 @@ function submitAnswers() {
 	var q4 = document.forms["quizForm"]["q4"].value;
 	var q5 = document.forms["quizForm"]["q5"].value;
 
-	// Validation
 	
 
+	// Validation
 	for(i = 1;i <= total;i++) {
 
 			if(eval('q' + i) == null || eval('q' + i) == '') {
@@ -20,5 +22,33 @@ function submitAnswers() {
 		}
 	
 	}
+
+
+
+	// Set the correct answers
+	var answers = ["b", "a", "d", "b", "d"];
+
+	// Check answers 
+	if(q1 == answers[0]) {
+		score ++;
+	}
+
+	if(q2 == answers[1]) {
+		score ++;
+	}
+
+	if(q3 == answers[2]) {
+		score ++;
+	}
+
+	if(q4 == answers[3]) {
+		score ++;
+	}
+
+	if(q5 == answers[4]) {
+		score ++;
+	}
+
+	alert('You scored ' + score+ ' out of ' + total);
 
 }
