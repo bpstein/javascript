185207ea579efaeca13ea4c85350336030a68838
Ben Stein <bumstain@gmail.com>
2015-09-22 23:42:24 +0100

    Refactored code and finalised content slider project

--- content_slider/js/script.js
+++ content_slider/js/script.js
@@ -13,8 +13,19 @@ $(document).ready(function(){
 	// Show first slide
 	$('.active').show();
 
-	$('#next').on('click', function() {
+	// Next slide handler
+	$('#next').on('click', nextSlide);
 
+	// Previous slide handler
+	$('#prev').on('click', prevSlide);
+
+	// Auto slide handler
+	if(autoswitch == true) {
+		setInterval(nextSlide, autoswitch_speed);
+	}
+
+	// Switch to the next slide
+	function nextSlide() {
 		$('.active').removeClass('active').addClass('oldActive');
 	
 		if($('.oldActive').is(':last-child')){
@@ -28,12 +39,11 @@ $(document).ready(function(){
 		$('.oldActive').removeClass('oldActive');
 		$('.slide').fadeOut(speed);
 		$('.active').fadeIn(speed);
+	}
 
-	});
-
-	$('#prev').on('click', function() {
-
-		$('.active').removeClass('active').addClass('oldActive');
+	// Switch to the previous slide 
+	function prevSlide() {
+			$('.active').removeClass('active').addClass('oldActive');
 	
 		if($('.oldActive').is(':first-child')){
 			$('.slide').last().addClass('active');
@@ -46,24 +56,5 @@ $(document).ready(function(){
 		$('.oldActive').removeClass('oldActive');
 		$('.slide').fadeOut(speed);
 		$('.active').fadeIn(speed);
-
-	});
-
-	if(autoswitch == true) {
-		setInterval(function(){
-				$('.active').removeClass('active').addClass('oldActive');
-	
-		if($('.oldActive').is(':last-child')){
-			$('.slide').first().addClass('active');
-		}
-
-		else {
-			$('.oldActive').next().addClass('active');
-		}
-
-		$('.oldActive').removeClass('oldActive');
-		$('.slide').fadeOut(speed);
-		$('.active').fadeIn(speed);
-		}, autoswitch_speed);
 	}
 });
