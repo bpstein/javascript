f42d1ed07612122a80464a04a56913190c1e6b26
Ben Stein <bumstain@gmail.com>
2015-09-26 18:06:41 +0100

    Added mouseenter event  for projects

--- portfolio_gallery/js/script.js
+++ portfolio_gallery/js/script.js
@@ -30,4 +30,35 @@ $(document).ready(function(){
 		// Stop link behavior
 		return false;
 	});
+
+	$('ul#gallery li').on('mouseenter', function() {
+		
+		console.log('Mouse enter is working.');
+		
+		// Get data attributes from each list item
+		var title = $(this).children().data('title');
+		var desc = $(this).children().data('desc');
+
+		// Validation
+		if(desc == null) {
+			desc = 'Click to enlarge';
+		}
+
+		if(title == null) {
+			title = '';
+		}
+		
+		// Create overlay div 
+		$(this).append('<div class="overlay"></div>');
+
+		// Get overlay div 
+		var overlay = $(this).children('.overlay');
+
+		// Add HTML to overlay
+		overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+
+		// Fade in overlay 
+		overlay.fadeIn(1800);
+	});
+
 });
