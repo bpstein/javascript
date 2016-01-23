ca9975949e972d66fbceab91a409782558b93aea
Ben Stein <bumstain@gmail.com>
2016-01-23 00:30:21 +0000

    Added functionality to edit runs for Miletracker

--- jquery_miletracker/js/script.js
+++ jquery_miletracker/js/script.js
@@ -1,32 +1,40 @@
 $(document).one('pageinit', function(){
+
+
+	/****** EVENT HANDLERS ******/
 	// Display runs
 	showRuns();
 
-
 	// Add Handler
 	$('#submitAdd').on('tap', addRun);
-
-	/****** SHOW ALL RUNS ON HOMEPAGE *******/
-
-	function showRuns() {
-		// Get runs object
+	
+	// Edit Handler
+	$('#submitEdit').on('tap', editRun);
+	
+	// Set Current Handler
+	$('#stats').on('tap','#editLink', setCurrent);
+	
+	
+	/****** SHOW RUNS ON HOMEPAGE ******/
+	function showRuns(){
+		// get runs object
 		var runs = getRunsObject();
-
-		// Check if empty 
-		if(runs != '' && runs != null) {
-			for(var i = 0; i < runs.length; i++) {
-				$('#stats').append('<li class:"ui-body-inherit ui-li-static"><strong>Date:</strong>'+ ' ' + runs[i]["date"]+
-					'<br><strong>Distance: </strong>' + ' ' + runs[i]["miles"] + 'm<div class="controls">' + 
-					'<a href="#edit">Edit</a> | <a href="#delete">Delete</a></li>');
+		
+		// Check if empty
+		if(runs != '' && runs != null){
+			for(var i = 0;i < runs.length;i++){
+				$('#stats').append('<li class="ui-body-inherit ui-li-static"><strong>Date:</strong>'+runs[i]["date"]+
+				' <br><strong>Distance: </strong>'+runs[i]["miles"]+'m<div class="controls">' +
+				'<a href="#edit" id="editLink" data-miles="'+runs[i]["miles"]+'" data-date="'+runs[i]["date"]+'">Edit</a> | <a href="#">Delete</a></li>');
 			}
-			$('#home').bind('pageinit', function() {
-				$('stats').listview('refresh');
+			$('#home').bind('pageinit', function(){
+				$('#stats').listview('refresh');
 			});
 		}
 	}
-	
-	/****** ADD RUNS FUNCTIONALITY *******/
-	 function addRun(){
+	 
+	/****** ADD RUNS ******/
+	function addRun(){
 		// Get form values
 		var miles = $('#addMiles').val();
 		var date = $('#addDate').val();
@@ -42,7 +50,48 @@ $(document).one('pageinit', function(){
 		// Add run to runs array
 		runs.push(run);
 		
-		alert('The run has been added!');
+		alert('Run Added');
+		
+		// Set stringified object to localStorage
+		localStorage.setItem('runs', JSON.stringify(runs));
+		
+		// Redirect
+		window.location.href="index.html";
+		
+		return false;
+	}
+	 
+	 
+	/****** EDIT RUNS ******/
+	function editRun(){
+		// Get current data
+		currentMiles = localStorage.getItem('currentMiles');
+		currentDate = localStorage.getItem('currentDate');
+		
+		var runs = getRunsObject();
+		
+		// Loop through runs
+		for(var i = 0;i < runs.length;i++){
+			if(runs[i].miles == currentMiles && runs[i].date == currentDate){
+				runs.splice(i,1);
+			}
+			localStorage.setItem('runs',JSON.stringify(runs));
+		}
+		
+		// Get form values
+		var miles = $('#editMiles').val();
+		var date = $('#editDate').val();
+		
+		// Create 'run' object
+		var update_run = {
+			date: date,
+			miles: parseFloat(miles)
+		};
+		
+		// Add run to runs array
+		runs.push(update_run);
+		
+		alert('Run Updated');
 		
 		// Set stringified object to localStorage
 		localStorage.setItem('runs', JSON.stringify(runs));
@@ -53,10 +102,8 @@ $(document).one('pageinit', function(){
 		return false;
 	 }
 	 
-
-
 	 
-	 /****** GET RUNS OBJECT *******/
+	/****** GET THE RUNS OBJECT ******/
 	 function getRunsObject(){
 		// Set runs array
 		var runs = new Array();
@@ -68,10 +115,19 @@ $(document).one('pageinit', function(){
 			// Set to runs
 			var runs = JSON.parse(currentRuns);
 		}
-
-
 		
 		// Return runs object
 		return runs.sort(function(a, b){return new Date(b.date) - new Date(a.date)});
+	 }
+	 
+	/****** SET THE CURRENT MILES AND DATE ******/
+	 function setCurrent(){
+		// Set ls items
+		localStorage.setItem('currentMiles', $(this).data('miles'));
+		localStorage.setItem('currentDate', $(this).data('date'));
+		
+		// Insert form fields
+		$('#editMiles').val(localStorage.getItem('currentMiles'));
+		$('#editDate').val(localStorage.getItem('currentDate'));
 	 }
 });
